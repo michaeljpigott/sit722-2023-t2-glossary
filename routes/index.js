d93f66ad828331fae7c8877031fe7183b2bcd4be
Michael Pigott <michael.pigott82@gmail.com>
2023-07-31 09:02:04 +1000

    Added glossary terms up to 20

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const glossaryList = [
     id: "1",
     term: "CI/CD pipelines",
     description:
-      "A workflow where a combined development and operations team (DevOps) can seamlessly update software in a production environment.\n Use automated testing for quality control.  ",
+      "Stands for continuous integration and continuous deployment pipelines. \n A workflow where a combined development and operations team (DevOps) can seamlessly update software in a production environment.\n Uses automated testing for quality control.  ",
     references:
       "Red Hat (11 May 2022) What is a CI/CD pipeline?, Red Hat, accessed on 5 July 2022. https://www.redhat.com/en/topics/devops/what-cicd-pipeline \n IBM Cloud Education (27 September 2021) What Are CI/CD and the CI/CD Pipeline?, IBM, accessed on 16 July 2023. https://www.ibm.com/cloud/blog/ci-cd-pipeline \n ",
   },
@@ -97,6 +97,70 @@ const glossaryList = [
       "This is an idea where you create your infrastructure as code rather than relying on an out-of-the-box point and click service. \n Using IAC means it is easier to replicate and share the configuration of your infrastructure, thus achieving better standardisation. \n",
     references:
       "Mulder J (2021) Enterprise DevOps for Architects, 1st edn, Packt Publishing, Birmingham UK. \n Prisma Cloud by Palo Alto Networks (2 July 2022) What Is Infrastructure as Code (IaC)? The Foundation of a Trusted Cloud Native Solution, Prisma Cloud by Palo Alto Networks, Youtube, accessed on 24 July 2023. https://www.youtube.com/watch?v=og3e9OJJ4Us \n PulumiTV (4 November 2021) Infrastructure as Code | Quick Bites of Cloud Engineering (QB1), PulumiTV, YouTube, accessed on 24 July 2023. https://www.youtube.com/watch?v=WhWf48kcEXU \n ",
+  },
+  {
+    id: "13",
+    term: "Minimum Viable Product (MVP)",
+    description:
+      "A bare bones version of a product that works in production. \n The development team can present this product to the business owners. \n ",
+    references:
+      "Mailchimp (n.d.) What You Need to Know About Minimum Viable Product (MVP), Mailchimp, accessed on 30 July 2023. https://mailchimp.com/resources/minimum-viable-product/ \n Mulder J (2021) Enterprise DevOps for Architects, 1st edn, Packt Publishing, Birmingham UK. \n ",
+  },
+  {
+    id: "14",
+    term: "User story",
+    description:
+      "A story written from a user's perspective that describes what they want the product to do. \n They use non-technical language. \n ",
+    references:
+      "Mulder J (2021) Enterprise DevOps for Architects, 1st edn, Packt Publishing, Birmingham UK. \n Rehkopf M (n.d.) User stories with examples and a template, Atlassian, accessed on 30 July 2023. https://www.atlassian.com/agile/project-management/user-stories. \n ",
+  },
+  {
+    id: "15",
+    term: "Continuous integration (CI)",
+    description:
+      "A software practice where a team of developers continuously integrate their code in source code manager, e.g. Github. \n The source code manager will automatically check for conflicts. \n ",
+    references:
+      "Atlasssian (n.d.) What is continuous integration? Atlassian, accessed on 30 July 2022. https://www.atlassian.com/continuous-delivery/continuous-integration \n Krief M (2022) Learning DevOps, 2nd edn, Packt Publishing, Birmingham UK. \n ",
+  },
+  {
+    id: "16",
+    term: "Continuous delivery (CD)",
+    description:
+      "The process where the application is moved into a non-production environment, e.g. staging. \n In a microservices environment, this is where one microservice is tested with all the other microservices. \n Compatability and dependencies will be tested. \n ",
+    references:
+      "AWS (n.d.) What is Continuous Delivery? AWS, accessed on 30 July 2022. https://aws.amazon.com/devops/continuous-delivery/ \n Krief M (2022) Learning DevOps, 2nd edn, Packt Publishing, Birmingham UK. \n ",
+  },
+  {
+    id: "17",
+    term: "Docker",
+    description:
+      "A software platform that allows you to place an application in a container. This container can then run on any machine regardless of its infrastucture. \n ",
+    references:
+      "AWS (n.d.) What is Docker? AWS, accessed on 30 July 2022. https://aws.amazon.com/docker/ \n Vadapalli S (2018) DevOps: Continuous Delivery, Integration, and Deployment with DevOps, 1st edn, Packt Publishing, Birmingham UK. \n ",
+  },
+  {
+    id: "18",
+    term: "Kubernetes",
+    description:
+      "Manages containers through an application programming interface (API). \n Can replicate containers and deploy them on physical machines. ",
+    references:
+      "AWS (n.d.) What is Kubernetes? Azure, accessed on 30 July 2022. https://azure.microsoft.com/en-au/resources/cloud-computing-dictionary/what-is-kubernetes/#overview \n Vadapalli S (2018) DevOps: Continuous Delivery, Integration, and Deployment with DevOps, 1st edn, Packt Publishing, Birmingham UK. \n ",
+  },
+  {
+    id: "19",
+    term: "Container",
+    description:
+      "Can virtualise a service by placing it in a box. \n Docker is an example of a software platform that creates containers. \n Unlike virtual machines, they don't need to replicate a virtual operating system. They are more lightweight than virtual machines. \n ",
+    references:
+      "Davis A (2021) Bootstrapping microservices with Docker, Kubernetes, and Terraform: a project-based guide, 1st edn, Manning Publications Co., Shelter Island New York. \n ",
+  },
+  {
+    id: "20",
+    term: "Image",
+    description:
+      "A snapshot of a server or microserver that cannot be modified. \n A container is booted from an image. \n ",
+    references:
+      "Davis A (2021) Bootstrapping microservices with Docker, Kubernetes, and Terraform: a project-based guide, 1st edn, Manning Publications Co., Shelter Island New York. \n ",
   },
 ];
 
